4a7ea2a286d113dce31c98c2c858f546582bcd02
Reiter Timon <timon.reiter@gmail.com>
2021-11-28 16:15:07 +0100

    + User Model

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/User.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/User.java
@@ -1,4 +1,77 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.lang.model.element.Name;
+import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+@Entity
 public class User {
+    private Name name;
+    private String username;
+    private LocalDate birthDate;
+    private List<Permission> permissions;
+    private String Firstname;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    private String Lastname;
+
 }
