180800286c2947cc96a72d36feb8d8f6df11b01d
Bernhard Siegl <sie19038@spengergasse.at>
2021-11-28 15:09:44 +0100

    + GameContent model

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/GameContent.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/GameContent.java
@@ -1,5 +1,29 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
 public class GameContent {
 
+    private Long id;
+    private List<Item> itemsInGame;
+    private List<Mob> mobsInGame;
+    private List<NonPlayerCharacter> nonPlayerCharactersInGame;
+    private User loggedInUser;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
 }
