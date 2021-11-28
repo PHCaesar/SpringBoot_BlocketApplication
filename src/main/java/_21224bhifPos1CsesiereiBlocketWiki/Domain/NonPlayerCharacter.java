35280c69629914e41c73f5b2f9b07f81208a7f0c
Senpai-Sinpai <timon.reiter@gmail.com>
2021-11-28 15:30:17 +0100

    + NonPlayerCharacterModel

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/NonPlayerCharacter.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/NonPlayerCharacter.java
@@ -1,5 +1,47 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
 public class NonPlayerCharacter {
+
+    private String name;
+    private float health;
+    private List<Item> shopItems;
+
+    @Id
+    @GeneratedValue
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public List<Item> getShopItems() {
+        return shopItems;
+    }
+
+    public void setShopItems(List<Item> shopItems){
+        this.shopItems=shopItems;
+    }
+
+
+    public float getHealth(){
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
 
 }
