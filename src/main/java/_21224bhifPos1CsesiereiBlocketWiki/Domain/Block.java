a846bb39c481b69948edd3255f4eba929372114c
Bernhard Siegl <sie19038@spengergasse.at>
2021-11-28 14:52:43 +0100

    + Block model

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Block.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Block.java
@@ -1,5 +1,28 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
 public class Block {
+
+    private long id;
+    private String name;
+    private int size;
+    private int blockDurability;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
 
 }
