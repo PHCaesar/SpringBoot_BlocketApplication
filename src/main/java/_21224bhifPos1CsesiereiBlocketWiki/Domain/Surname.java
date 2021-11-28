203b89bf7a7f9aee4beefb2d478df8e164f563a4
Reiter Timon <timon.reiter@gmail.com>
2021-11-28 15:59:02 +0100

    + SurnameModel

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Surname.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Surname.java
@@ -1,4 +1,31 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Embeddable
 public class Surname {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
 }
