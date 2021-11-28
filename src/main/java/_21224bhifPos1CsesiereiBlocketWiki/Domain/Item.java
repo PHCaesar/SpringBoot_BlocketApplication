d7aba18fa25e4e8392b4c28a5bda252b3dbc65ae
Bernhard Siegl <sie19038@spengergasse.at>
2021-11-28 15:01:56 +0100

    + Item model

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Item.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Item.java
@@ -1,5 +1,24 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
 public class Item {
 
+    private Long id;
+    private String name;
+    private int size;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
 }
