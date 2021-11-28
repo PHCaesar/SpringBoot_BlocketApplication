8e4dd57bf3002c12773073f89340b396d333c829
unknown <cse19455@spengergasse.at>
2021-11-28 15:09:54 +0100

    Mob Model

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Mob.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Mob.java
@@ -2,30 +2,40 @@ package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Entity
 @NoArgsConstructor
 public class Mob {
 
-    private Long id;
+    private long id;
     private String name;
     private List<Item> drops;
     private MobType type;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    Predicate<MobType> isFriendly = vl -> vl.equals(MobType.FRIENDLY);
+    Predicate<MobType> isAggressive = vl -> vl.equals(MobType.AGGRESSIVE);
+    Predicate<List<Item>> hasDrops = vl -> vl.size()!=0;
+
 
     @Id
     @GeneratedValue
-    public Long getId() {
+    public long getId() {
         return id;
     }
-
-
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() { return name; }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Item> getDrops() { return drops; }
+    public void setDrops(List<Item> drops) { this.drops = drops; }
+    public MobType getType() { return type; }
+    public void setType(MobType type) { this.type = type; }
 }
