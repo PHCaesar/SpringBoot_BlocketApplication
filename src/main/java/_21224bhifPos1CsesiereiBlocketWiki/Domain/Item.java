70882e3f60b97b96195f8a8f6112f95ecef3d0f1
Bernhard Siegl <sie19038@spengergasse.at>
2021-12-05 11:20:25 +0100

    + AllArgsConstructer and + @Getter@Setter

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Item.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Domain/Item.java
@@ -1,23 +1,24 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Domain;
 
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.function.Predicate;
 
 @NoArgsConstructor
+@AllArgsConstructor
 public class Item {
 
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private int size;
 
     Predicate<String> hasName = vl -> !vl.isBlank();
     Predicate<Integer> hasSize = vl -> vl.intValue()!=0;
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     public Long getId() {
         return id;
     }
@@ -25,16 +26,4 @@ public class Item {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getSize() {
-        return size;
-    }
-    public void setSize(int size) {
-        this.size = size;
-    }
 }
