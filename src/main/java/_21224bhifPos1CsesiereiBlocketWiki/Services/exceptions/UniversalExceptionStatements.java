8bbd2a266442bb8bfda1c14d69d1e1491b693455
Cserich Philipp <cse19455@spengergasse.at>
2022-04-30 17:20:51 +0000

    Merged in feature/AsciiDoc (pull request #22)
    Feature/AsciiDoc
    
    * AsciiDoc reference - HTML processing
    
    * Pdf convertation
    
     
    
    * remove target folder
    
    * Merged development into feature/AsciiDoc

--- src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Services/exceptions/UniversalExceptionStatements.java
+++ src/main/java/_21224bhifPos1CsesiereiBlocketWiki/Services/exceptions/UniversalExceptionStatements.java
@@ -1,10 +1,12 @@
 package _21224bhifPos1CsesiereiBlocketWiki.Services.exceptions;
 
 public enum UniversalExceptionStatements {
+    //tag::UniversalExceptions[]
     BLANK_OR_EMPTY_MSG("cannot be empty or blank!"),
     DATA_NOT_FOUND("cannot be accessed or does not exist!"),
     DUPLICATE_DATA_FOUND("is already created!"),
     WRONG_RANGE_INPUT(" is not in the expected range!");
+    //end::UniversalExceptions[]
 
     UniversalExceptionStatements(String msg){
         this.msg = msg;
